276e7e0adbe465186f8e2937ff16b4808abb99b9
zulkernine <53091788+zulkernine@users.noreply.github.com>
2019-11-14 02:04:44 +0530

    Update two_vowel_occurence.c

--- two_vowel_occurence.c
+++ two_vowel_occurence.c
@@ -1,22 +1,20 @@
 #include<stdio.h>
-void main(){
+int is_vowel(char ch){
     char vowel[10]={'a','e','i','o','u','A','E','I','O','U'};
+    for(int i=0;i<10;i++){
+        if(ch == vowel[i])
+        return 1;
+    }
+    return 0;
+}
+void main(){
     char text[100];
     int count=0;
     printf("Enter text(within 100 charecter) =");
     scanf("%[^\n]s",text);
-    for(int i=0;text[i+1];i++){
-        int succesion =0;
-        for(int j=0;vowel[j];j++){
-            if(text[i]==vowel[j])
-                succesion += 1;
-            if(text[i+1]==vowel[j])
-                succesion += 1;
-            
-        }
-        if(succesion==2)
+    for(int i=0;text[i+1]!='\0';i++){
+        if(is_vowel(text[i])&&is_vowel(text[i+1]))
             count += 1;
     }
-    printf("Number of ocurance of any two vowel in succesion = %d\n",count); 
-
+    printf("Number of ocurance of any two vowel in succesion = %d\n",count);
 }
