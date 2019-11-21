dbf944141cae7fb3f542629c5997a41f2f8918b2
zulkernine <53091788+zulkernine@users.noreply.github.com>
2019-11-22 01:39:54 +0530

    Update telephone_directory.c

--- telephone_directory.c
+++ telephone_directory.c
@@ -5,6 +5,14 @@ typedef struct directory_info{
     char name[20];
     char number[3][12];
 } directory_info_t;
+void rearrange(directory_info_t *str,int i,int legit){
+    str[i] = str[legit-1];
+    //delete_contact(str,legit);
+    str[legit].name[0]='\0';
+    str[legit].number[0][0]='\0';
+    str[legit].number[1][0]='\0';
+    str[legit].number[2][0]='\0';
+}
 int find_free(directory_info_t *str){
     for(int i=0;i<50;i+=1){
         if(str[i].name[0]=='\0')
@@ -12,24 +20,21 @@ int find_free(directory_info_t *str){
     }
     return 0;
 }
-void browse(directory_info_t*str){
+void browse(directory_info_t*str,int N){
     printf("\n---------------------------------\nPRINTING CONTACT INFORMATION:\n");
-    for(int i=0;i<50;i++){
-        if(str[i].name[0]!='\0'){
+    for(int i=0;i<N;i++){
         printf("%d.\nContact Name : %s\n",i+1,str[i].name);
         for(int j=0;j<3;j++)
             if(str[i].number[j][0]!='\0')
                 printf("Contact Number %d = %s \n",j+1,str[i].number[j]);
             else 
-                break;
-        }
-        else
-            break;        
+                break;      
         printf("-------------------------------\n");
     } 
 }
 int main(){
     directory_info_t file[50];
+    int file_size=0;
     //initialise the structure
     for(int i=0;i<50;i++){
         file[i].name[0]='\0';
@@ -45,6 +50,7 @@ int main(){
         getchar();
         switch(ch){
             case '1' :{
+                printf("\n----------------------------------\nADD CONTACT\n");
                 int k=find_free(file);
                 printf("Enter name:");
                 scanf("%[^\n]s",(file + k)->name);
@@ -54,30 +60,43 @@ int main(){
                     scanf("%[^\n]s",(file + k)->number[i]);
                     getchar();
                 }
+                file_size += 1;
+                printf("\n------------------------------------------\n");
             };break;
             case '2' :{
+                printf("\n------------------------------------------\n");
+                printf("DELETE CONTACT\n");
                 char str[20]; 
                 printf("Enter contact name: ");
                 scanf("%[^\n]s",str);
-                for(int i=0;i<50;i++){
+                for(int i=0;i<file_size;i++){
                     if(!strcmp(str,(file + i)->name)){
-                        (file + i)->name[0] = '\0';
+                        //(file + i)->name[0] = '\0';
+                        //delete_contact(file,i);
+                        rearrange(file,i,file_size);
+                        file_size -= 1;
                         break;
                     }
                 }
+                printf("\n------------------------------------------\n");
             };break;
             case '3': {
+                printf("\n------------------------------------------\n");
+                printf("SEARCH NAME\n");
                 char sname[20];
-                for(int i=0;i<50;i++){
+                printf("Enter the name:");
+                scanf("%[^\n]s",sname);
+                for(int i=0;i<file_size;i++){
                     if(!strcmp(sname,file[i].name)){
                         printf("Number of %s is:\n",file[i].name);
-                        for(int j=0;file[i].number[j];j++)
+                        for(int j=0;file[i].number[j][0]!='\0';j++)
                             printf("%s\n",file[i].number[j]);
                         break;
                     }
-                }                
+                }
+                printf("\n------------------------------------------\n");                
             };break;
-            case '4':browse(file);break;
+            case '4':browse(file,file_size);break;
             case '5':exit(0);break;
         }
     }
