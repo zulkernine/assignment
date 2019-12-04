253f57295db1384f814b12c1da27e3a7453e9fc1
Mohiuddin Mondal <mohiuddinmondal107@gmail.com>
2019-12-05 00:53:55 +0530

    Update str_cat.c

--- str_cat.c
+++ str_cat.c
@@ -1,18 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-int main(){
-    char str1[50],str2[50],str3[100];
+char*str_alt(char*str1,char*str2){
+    static char str3[100];
     int i=0,j=0,k=0;
-    printf("Enter the first string:");
-    scanf("%[^\n]s",str1);
-    //fflush(stdout);
-    while(getchar()!='\n');
-    printf("%s\n",str1);
-    printf("Enter second string:");
-    scanf("%[^\n]s",str2);
-    //gets(str2);
-    printf("%s\n",str2);
     for(i;str1[j]||str2[k];i++){
         if(i%2==0 && str1[j]){
             str3[i] = str1[j];
@@ -30,6 +20,20 @@ int main(){
         }
     }
     str3[i]='\0';
-    printf("new string = %s\n",str3);
+    return str3;
+}
+int main(){
+    char str1[50],str2[50];
+    printf("Enter the first string:");
+    scanf("%[^\n]s",str1);
+    //fflush(stdout);
+    while(getchar()!='\n');
+    //printf("%s\n",str1);
+    printf("Enter second string:");
+    scanf("%[^\n]s",str2);
+    //gets(str2);
+    //printf("%s\n",str2);
+    
+    printf("new string = %s\n",str_alt(str1,str2));
     return 0;
 }
