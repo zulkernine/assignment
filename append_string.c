be8bbb165cc81f1751e258a8ad6952cbe648e330
Mohiuddin Mondal <mohiuddinmondal107@gmail.com>
2019-12-05 01:15:10 +0530

    Update append_string.c

--- append_string.c
+++ append_string.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 char *str_append(char*s1,char*s2,int i){
-    char* s3=malloc(60*sizeof(char));
+    static char* s3=malloc(60*sizeof(char));
     int j=0;
     if(!s3){
         printf("Exit");
