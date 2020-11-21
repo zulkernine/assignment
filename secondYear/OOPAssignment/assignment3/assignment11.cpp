a5bf800fe86842878559df15ea8959006c3429f6
zulkernine <mohiuddinmondal107@gmail.com>
2020-11-21 11:52:03 -0800

    oopassignment assignment3/10

---
+++ secondYear/OOPAssignment/assignment3/assignment11.cpp
@@ -0,0 +1,7 @@
+/*
+    Design a STUDENT class to store roll, name, course, admission date and marks in 5 subjects. 
+    Provide methods corresponding to admission (marks are not available then), 
+    receiving marks and preparing mark sheet. 
+    Support must be there to show the number of students who have taken admission.
+
+*/
