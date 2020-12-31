5448118e07f79adc1bc495d514633c579f321a61
zulkernine <mohiuddinmondal107@gmail.com>
2021-01-01 01:43:39 +0530

    Sorry but that bug just destroyed my 2021 new year moment

--- secondYear/DSAAssignment/assignment2/q2.c
+++ secondYear/DSAAssignment/assignment2/q2.c
@@ -3,27 +3,28 @@
 void inputMatrix(matrix_term* matrix){
     printf("Eneter row column and number of non-zero terms space separated(r c n): ");
     scanf("%d %d %f", &(matrix[0].row), &(matrix[0].col), &(matrix[0].value));
-    printf("Enter %d terms space separated, each in a new line:\n",(int)matrix[0].value);
+    printf("Enter %d terms space separated, each in a new line:\n", (int)matrix[0].value);
     for (int i = 1;i <= matrix[0].value;i++){
         printf("row col value : ");
-        scanf("%d %d %f",&(matrix[i].row),&(matrix[i].col),&(matrix[i].value));
+        scanf("%d %d %f", &(matrix[i].row), &(matrix[i].col), &(matrix[i].value));
     }
 }
 
 int main(){
-    matrix_term *mtx1,*mtx2,*addm,*transm,*mult;
+    matrix_term* mtx1, * mtx2, * addm, * transm, * mult;
 
-    mtx1=create_matrix(); mtx2=create_matrix();
+    mtx1 = create_matrix(); mtx2 = create_matrix();
     inputMatrix(mtx1);inputMatrix(mtx2);
 
-    printf("Matrix1:\n");print_matrix(mtx1); 
+    printf("Matrix1:\n");print_matrix(mtx1);
     printf("Matrix2:\n");print_matrix(mtx2);
 
-    addm = add_matrix(mtx1,mtx2);
-    if(addm){
+    addm = add_matrix(mtx1, mtx2);
+    if (addm){
         printf("\n\nAdded matrix:\n\n");
         print_matrix(addm);
-    }else{
+    }
+    else{
         printf("Given matrix can not be added\n");
     }
 
@@ -33,15 +34,15 @@ int main(){
 
     printf("Matrix1:\n");print_matrix(mtx1);
     printf("Matrix2:\n");print_matrix(mtx2);
-    
-    mult = multiply_matrix(mtx1,mtx2);
-    if(mult){
+
+    mult = multiply_matrix(mtx1, mtx2);
+    if (mult){
         printf("\n\nMultiplication of matrix1 and marix2:\n\n");
         print_matrix(mult);
-    }else{
+    }
+    else{
         printf("Given matrix can't be multiplied\n");
     }
-    
 
     printf("\n");
 
