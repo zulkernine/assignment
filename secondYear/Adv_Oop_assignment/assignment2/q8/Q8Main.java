913fd13bd0c391840650f7dd038f0a64dea96a8f
zulkernine <mohiuddinmondal107@gmail.com>
2021-05-04 15:34:14 +0530

    assembly

---
+++ secondYear/Adv_Oop_assignment/assignment2/q8/Q8Main.java
@@ -0,0 +1,5 @@
+class Q8Main{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ secondYear/Adv_Oop_assignment/assignment2/q8/RegisterEmployee.java
@@ -0,0 +1,14 @@
+public class RegisterEmployee {
+    private Employee emp;
+    RegisterEmployee(){
+        emp = new Employee();
+    }
+
+    public Employee getEmp() {
+        return emp;
+    }
+
+    
+
+
+}
