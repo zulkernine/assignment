8989e30120fd05edb0935b210e5ef395990c8a26
zulkernine <mohiuddinmondal107@gmail.com>
2022-01-19 09:16:10 +0530

    minor bug fixed

--- thirdYear/Internet Technology/assignment1/store/KeyFetchClient.java
+++ thirdYear/Internet Technology/assignment1/store/KeyFetchClient.java
@@ -9,19 +9,19 @@ import java.util.Scanner;
 
 public class KeyFetchClient {
     public static void main(String[] args) {
-        try{
-            if(args.length<1){
+        try {
+            if (args.length < 1) {
                 System.out.println("Please provide port number (except 8000)");
                 System.exit(1);
             }
 
             int port = Integer.parseInt(args[0]);
             // if(port == 8000){
-            //     System.out.println("Please provide port number except 8000");
-            //     System.exit(1);
+            // System.out.println("Please provide port number except 8000");
+            // System.exit(1);
             // }
 
-            Socket server = new Socket("127.0.0.1",port);
+            Socket server = new Socket("127.0.0.1", port);
             InputStream in = server.getInputStream();
             OutputStream out = server.getOutputStream();
 
@@ -37,29 +37,30 @@ public class KeyFetchClient {
 
             System.out.println(initialMessage);
 
-            interactWithUser(server,response,request);
+            interactWithUser(server, response, request);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    private static void interactWithUser(Socket sc,DataInputStream response, DataOutputStream request){
+    private static void interactWithUser(Socket sc, DataInputStream response, DataOutputStream request) {
         Scanner scanner = new Scanner(System.in);
 
-        while(sc.isConnected() && (!sc.isOutputShutdown() && !sc.isInputShutdown())){
-            System.out.print("remote> ");
-            String command = scanner.nextLine();
-            try {
+        try {
+            while (true) {
+                System.out.print("remote> ");
+                String command = scanner.nextLine();
                 request.writeUTF(command);
                 request.flush();
 
                 System.out.println(response.readUTF());
-            } catch (Exception e) {
-                System.out.println(e);
-            }
 
-            System.out.println();
+                System.out.println();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Connection closed");
         }
 
         scanner.close();
